86e9308e775e3f5572ebf963f456a2164731590d
Dominik Vogel <xpossseidon@gmail.com>
2016-10-05 11:50:22 +0200

    Added out Solution for SoftwareEngineering and a Test Project you can use to test some small snippets of code

---
+++ SoftwareEngineering/Testing/Testing.cpp
@@ -0,0 +1,21 @@
+// Testing.cpp : Defines the entry point for the console application.
+//
+// This is just a testproject you can use for any small tests
+//
+
+#include "stdafx.h"
+
+
+using namespace std;
+
+int main()
+{
+    cout << "Test Start" << endl;
+
+    cout << "Test End!" << endl;
+
+    system("pause");
+
+    return 0;
+}
+
---
+++ SoftwareEngineering/Testing/stdafx.h
@@ -0,0 +1,17 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+#include <iostream>
+
+// install this, guys, it's really useful!
+#include <vld.h>       
+
