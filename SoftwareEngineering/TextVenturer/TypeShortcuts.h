f84f8bdb24012d8f4421a1e83b051c98e4e0f51e
Possseidon <xpossseidon@gmail.com>
2017-05-05 13:25:42 +0200

    fixed ref_idlist and ref_unordered_map

--- SoftwareEngineering/TextVenturer/TypeShortcuts.h
+++ SoftwareEngineering/TextVenturer/TypeShortcuts.h
@@ -8,10 +8,10 @@ template<typename T>
 using idlist = std::unordered_map<T, UINT>;
 
 template<typename T>
-using ref_idlist = idlist<std::reference_wrapper<T>, UINT>;
+using ref_idlist = idlist<std::reference_wrapper<T>>;
 
 template<typename T>
 using ref_vector = std::vector<std::reference_wrapper<T>>;
 
-template<typename T>
-using ref_unordered_map = std::unordered_map<std::reference_wrapper<T>>;
+template<typename K, typename V>
+using ref_unordered_map = std::unordered_map<K, std::reference_wrapper<V>>;
