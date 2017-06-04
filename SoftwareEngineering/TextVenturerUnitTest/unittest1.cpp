a0946c34a343b70ac9109ae5169bb775da7973a0
Andre Schmitt <andre1502@gmx.de>
2017-06-04 15:13:18 +0200

    added del test

--- SoftwareEngineering/TextVenturerUnitTest/unittest1.cpp
+++ SoftwareEngineering/TextVenturerUnitTest/unittest1.cpp
@@ -1,8 +1,8 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 
-#include <stdafx.h>
-#include <AliasList.h>
+#include <../TextVenturer/stdafx.h>
+#include <../TextVenturer/AliasList.h>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -12,11 +12,19 @@ namespace TextVenturerUnitTest
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(Testadd)
 		{
             AliasList a;
             a.add("test");
             Assert::IsTrue(a.has("test"));
+		}
+
+		TEST_METHOD(Testdel)
+		{
+			AliasList a;
+			a.add("test");
+			a.del("test");
+			Assert::IsFalse(a.has("test"));
 		}
 
 	};
