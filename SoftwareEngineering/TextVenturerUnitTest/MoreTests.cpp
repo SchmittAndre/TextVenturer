231dfcf4539aa9b57c49335540f3ee90aeebea6c
Dominik Vogel <xpossseidon@gmail.com>
2017-06-07 14:25:29 +0200

    added all obj files to UnitTesting and removed 4 unused files I found doing so

--- SoftwareEngineering/TextVenturerUnitTest/MoreTests.cpp
+++ SoftwareEngineering/TextVenturerUnitTest/MoreTests.cpp
@@ -1,108 +1,22 @@
 #include "stdafx.h"
 
-#include <ErrorHandling.h>
-#include <AliasList.h>
-#include <StringUtils.h>
+#include <Location.h>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace AliasListTest
+namespace LocationTest
 {
-    TEST_CLASS(AliasListTest)
+    TEST_CLASS(LocationTest)
     {
-    public:
-        const std::string Aliaslistname = "Aliaslist";
-        enum TestMethodID
+
+        TEST_METHOD(BasicTest)
         {
-            tmUnknown,
-            tmFile
-        };
+            Location loc;
+            loc.getAliases().add("tree");
+            loc.getAliases().add("big tree");
+            loc.setDescription("a big tree");
 
-        TestMethodID testMethod = TestMethodID::tmUnknown;
-
-        TEST_METHOD(TestAdd)
-        {
-            AliasList a;
-            a.add("test");
-            Assert::IsTrue(a.has("test"));
-        }
-
-        TEST_METHOD(TestDel)
-        {
-            AliasList a;
-            a.add("test");
-            a.del("test");
-            Assert::IsFalse(a.has("test"));
-        }
-
-        TEST_METHOD(TestDelWithWrongstring)
-        {
-            AliasList a;
-            a.add("test");
-            a.del("tes");
-            Assert::IsTrue(a.has("test"));
-        }
-
-        TEST_METHOD(TestAddDuplicate)
-        {
-            AliasList a;
-            a.add("test");
-            Assert::IsFalse(a.add("test"));
-        }
-
-        TEST_METHOD(TestGetName)
-        {
-            AliasList a;
-            a.add("test");
-            a.add("something else");
-            std::string name = a.getName();
-            bool assert = name == "a test";
-            Assert::IsTrue(assert);
-        }
-        TEST_METHOD(TestGetNameonly)
-        {
-            AliasList a;
-            a.add("test");
-            a.add("something else");
-            std::string name = a.getNameOnly();
-            bool assert = name == "test";
-            Assert::IsTrue(assert);
-        }
-
-        TEST_METHOD(File)
-        {
-            testMethod = TestMethodID::tmFile;
-            FileStream fs(strconv(Aliaslistname), std::ios::out);
-            AliasList a;
-            a.add("test");
-            a.add("tests", true);
-            a.add("hallo");
-            a.save(fs);
-            fs.close();
-
-            AliasList b;
-            FileStream fsIn(strconv(Aliaslistname), std::ios::in);
-            b.load(fsIn);
-            Assert::IsTrue(b.has("test"));
-            Assert::IsTrue(b.has("tests"));
-            Assert::IsTrue(b.has("hallo"));
-            fsIn.close();
-        }
-
-        TEST_METHOD_CLEANUP(cleanup)
-        {
-            switch (testMethod)
-            {
-            case TestMethodID::tmFile:
-                if (!DeleteFile(strconv(Aliaslistname).c_str()))
-                {
-                    Logger::WriteMessage(("Error at " + Aliaslistname + ": " + getErrorString(GetLastError())).c_str());
-                }
-                break;
-            default:
-
-                break;
-            }
+            Assert::IsTrue(loc.getAliases().has("the tree"));
         }
 
     };
