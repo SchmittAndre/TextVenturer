55c1e28f92b9dd7f0d4f789e255a664949a2a351
Dominik Vogel <xpossseidon@gmail.com>
2017-06-08 06:29:45 +0200

    actually working, but have to probably build glew myself, as it warns me, that it can't find the vc120.pdb of an older VS version

--- SoftwareEngineering/TextVenturer/Controler.cpp
+++ SoftwareEngineering/TextVenturer/Controler.cpp
@@ -51,7 +51,6 @@ void Controler::pressChar(byte c)
 
 void Controler::pressKey(byte key)
 {
-    OutputDebugStringA(("key " + std::to_string(key) + " with " + std::to_string(currentDisplayer) + "\r\n").c_str());
     getCurrentDisplayer().pressKey(key);
 }             
 
@@ -61,7 +60,6 @@ void Controler::update(float deltaTime)
 
     if (nextDisplayer != currentDisplayer)
     {
-        OutputDebugStringA(("change from " + std::to_string(currentDisplayer) + " to " + std::to_string(nextDisplayer) + "\r\n").c_str());
         getCurrentDisplayer().notifyUnload();
         textDisplay.clear();
         currentDisplayer = nextDisplayer;
